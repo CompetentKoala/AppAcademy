d20d9ba500205cc5a9a5aa2f4be429548412c9e0
Cameron <cameronkuy@gmail.com>
2024-02-16 18:37:45 -0800

    new files for Javascript

---
+++ Loops Prac./Matrix.js
@@ -0,0 +1,19 @@
+function maxInMatrix(matrix) {
+    let maxNumber = matrix[0][0]
+
+    for (let i = 0; i < matrix.length; i++){
+        let subMatrix = matrix[i]
+      for (let j = 0; j <subMatrix.length; j++){
+          if (subMatrix[j] > maxNumber){
+              maxNumber = subMatrix[j]
+          }
+      }
+    } return maxNumber
+  }
+
+
+  matrix = [[11,  2,-99],
+            [20, 19, 10],
+            [47, 72, 56]]
+
+  console.log(maxInMatrix(matrix)); // 72
---
+++ Loops Prac./nested loo.js
@@ -0,0 +1,10 @@
+function loop(arr){
+    for (let i = 0; i<arr.length; i++){
+
+        for (let j =i + 1; j<arr.length; j++){
+            console.log(arr[i],arr[j])
+        }
+    }
+}
+
+loop(["anna",'susan','john'])
